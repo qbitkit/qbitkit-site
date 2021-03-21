7badda32c593130865838aae5f8f8a203c952489
Theresa <theresasumma@gmail.com>
2021-03-20 21:17:12 -0700

    start initial global/homepage layout

---
+++ src/components/sitefooter.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const SiteFooter = () => {
+  return (
+    <div className="footer" id="footer">
+      <ul className="footerNav">
+        <li>Home</li>
+        <li><a href="https://qbitkit-docs-temporary.neutralthreat.com/docs/index.html">Documentation</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default SiteFooter
---
+++ src/components/siteheader.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SiteHeader = () => {
+  return (
+    <header className="main" id="main">
+      qbitkit
+    </header>
+  )
+}
+
+export default SiteHeader
